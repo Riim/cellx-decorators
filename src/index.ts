8a9199cb2708c66d3c27f96023368a0711a3114b
Riim <riim@yandex.ru>
2017-10-25 12:36:50 +0300

    add prettier

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import { assign } from '@riim/object-assign-polyfill';
 import { Cell, ICellOptions } from 'cellx';
 
-function enumerableDecorator(target: Object, name: string, desc?: PropertyDescriptor): any {
-	if (desc) {
-		desc.enumerable = true;
-		return desc;
+function enumerableDecorator(
+	target: Object,
+	propertyName: string,
+	propertyDesc?: PropertyDescriptor
+): any {
+	if (propertyDesc) {
+		propertyDesc.enumerable = true;
+		return propertyDesc;
 	}
 
 	return {
@@ -15,10 +19,14 @@ function enumerableDecorator(target: Object, name: string, desc?: PropertyDescri
 	};
 }
 
-function nonEnumerableDecorator(target: Object, name: string, desc?: PropertyDescriptor): any {
-	if (desc) {
-		desc.enumerable = false;
-		return desc;
+function nonEnumerableDecorator(
+	target: Object,
+	propertyName: string,
+	propertyDesc?: PropertyDescriptor
+): any {
+	if (propertyDesc) {
+		propertyDesc.enumerable = false;
+		return propertyDesc;
 	}
 
 	return {
@@ -32,7 +40,7 @@ function nonEnumerableDecorator(target: Object, name: string, desc?: PropertyDes
 /**
  * Babel PropertyDecorator arguments:
  * prototype
- * 'name'
+ * 'propertyName'
  * { configurable: false
  *   enumerable: true
  *   initializer: function initializer() | null
@@ -42,34 +50,39 @@ function nonEnumerableDecorator(target: Object, name: string, desc?: PropertyDes
  *
  * Typescript PropertyDecorator arguments:
  * prototype
- * 'name'
- * undefined | (результат предыдущего декоратора)
+ * 'propertyName'
+ * undefined | результат предыдущего декоратора
  *
  * Результат `'void' or 'any'`: https://github.com/Microsoft/TypeScript/issues/8063.
  *
  * AccessorDecorator arguments:
  * prototype
- * 'name'
+ * 'propertyName'
  * { configurable: true
  *   enumerable: true
  *   get: function ()
  *   set: undefined }
  */
-function CellDecorator<T = any>(target: Object, name: string, desc?: PropertyDescriptor): any;
-function CellDecorator<T = any>(opts: ICellOptions<T>):
-	(target: Object, name: string, desc?: PropertyDescriptor) => any;
+function CellDecorator<T = any>(
+	target: Object,
+	propertyName: string,
+	propertyDesc?: PropertyDescriptor
+): any;
+function CellDecorator<T = any>(
+	opts: ICellOptions<T>
+): (target: Object, propertyName: string, propertyDesc?: PropertyDescriptor) => any;
 function CellDecorator<T>(
 	targetOrOptions: Object | ICellOptions<T>,
-	name?: string,
-	desc?: PropertyDescriptor,
+	propertyName?: string,
+	propertyDesc?: PropertyDescriptor,
 	opts?: ICellOptions<T>
 ): any {
 	if (arguments.length == 1) {
-		return (target: Object, name: string, desc?: PropertyDescriptor): any =>
-			(CellDecorator as any)(target, name, desc, targetOrOptions);
+		return (target: Object, propertyName: string, propertyDesc?: PropertyDescriptor): any =>
+			(CellDecorator as any)(target, propertyName, propertyDesc, targetOrOptions);
 	}
 
-	let cellName = name + 'Cell';
+	let cellName = propertyName + 'Cell';
 
 	Object.defineProperty(targetOrOptions, cellName, {
 		configurable: true,
@@ -80,82 +93,122 @@ function CellDecorator<T>(
 
 	return {
 		configurable: true,
-		enumerable: desc ? desc.enumerable : true,
+		enumerable: propertyDesc ? propertyDesc.enumerable : true,
 
 		get(): any {
-			return (this[cellName] || (this[cellName] = new Cell(
-				desc && (desc.get || ((desc as any).initializer ? (desc as any).initializer() : desc.value)),
-				opts ? (opts.context === undefined ? assign({ context: this }, opts) : opts) : { context: this }
-			))).get();
+			return (this[cellName] ||
+				(this[cellName] = new Cell(
+					propertyDesc &&
+						(propertyDesc.get ||
+							((propertyDesc as any).initializer
+								? (propertyDesc as any).initializer()
+								: propertyDesc.value)),
+					opts
+						? opts.context === undefined ? assign({ context: this }, opts) : opts
+						: { context: this }
+				))
+			).get();
 		},
 
-		set: desc && desc.set || function(value: any) {
-			if (this[cellName]) {
-				this[cellName].set(value);
-			} else if (desc) {
-				(this[cellName] = new Cell(
-					desc.get || ((desc as any).initializer ? (desc as any).initializer() : desc.value),
-					opts ? (opts.context === undefined ? assign({ context: this }, opts) : opts) : { context: this }
-				)).set(value);
-			} else {
-				let isFn = typeof value == 'function';
-
-				this[cellName] = new Cell(
-					isFn ? value : undefined,
-					opts ? (opts.context === undefined ? assign({ context: this }, opts) : opts) : { context: this }
-				);
-
-				if (!isFn) {
+		set:
+			(propertyDesc && propertyDesc.set) ||
+			function(value: any) {
+				if (this[cellName]) {
 					this[cellName].set(value);
+				} else if (propertyDesc) {
+					(this[cellName] = new Cell(
+						propertyDesc.get ||
+							((propertyDesc as any).initializer
+								? (propertyDesc as any).initializer()
+								: propertyDesc.value),
+						opts
+							? opts.context === undefined ? assign({ context: this }, opts) : opts
+							: { context: this }
+					)).set(value);
+				} else {
+					let isFn = typeof value == 'function';
+
+					this[cellName] = new Cell(
+						isFn ? value : undefined,
+						opts
+							? opts.context === undefined ? assign({ context: this }, opts) : opts
+							: { context: this }
+					);
+
+					if (!isFn) {
+						this[cellName].set(value);
+					}
 				}
 			}
-		}
 	};
 }
 
-function observableDecorator<T = any>(target: Object, name: string, desc?: PropertyDescriptor): any;
-function observableDecorator<T = any>(opts: ICellOptions<T>):
-	(target: Object, name: string, desc?: PropertyDescriptor) => any;
+function observableDecorator<T = any>(
+	target: Object,
+	propertyName: string,
+	propertyDesc?: PropertyDescriptor
+): any;
+function observableDecorator<T = any>(
+	opts: ICellOptions<T>
+): (target: Object, propertyName: string, propertyDesc?: PropertyDescriptor) => any;
 function observableDecorator<T>(
 	targetOrOptions: Object | ICellOptions<T>,
-	name?: string,
-	desc?: PropertyDescriptor,
+	propertyName?: string,
+	propertyDesc?: PropertyDescriptor,
 	opts?: ICellOptions<T>
 ): any {
 	if (arguments.length == 1) {
-		return (target: Object, name: string, desc?: PropertyDescriptor): any =>
-			(observableDecorator as any)(target, name, desc, targetOrOptions);
+		return (target: Object, propertyName: string, propertyDesc?: PropertyDescriptor): any =>
+			(observableDecorator as any)(target, propertyName, propertyDesc, targetOrOptions);
 	}
 
-	if (desc && (desc.get || (desc.value !== undefined && typeof desc.value == 'function'))) {
+	if (
+		propertyDesc &&
+		(propertyDesc.get ||
+			(propertyDesc.value !== undefined && typeof propertyDesc.value == 'function'))
+	) {
 		throw new TypeError('Invalid descriptor of observable property');
 	}
 
-	return (CellDecorator as any)(targetOrOptions, name as string, desc, opts);
+	return (CellDecorator as any)(targetOrOptions, propertyName as string, propertyDesc, opts);
 }
 
-function computedDecorator<T = any>(target: Object, name: string, desc?: PropertyDescriptor): any;
-function computedDecorator<T = any>(opts: ICellOptions<T>):
-	(target: Object, name: string, desc?: PropertyDescriptor) => any;
+function computedDecorator<T = any>(
+	target: Object,
+	propertyName: string,
+	propertyDesc?: PropertyDescriptor
+): any;
+function computedDecorator<T = any>(
+	opts: ICellOptions<T>
+): (target: Object, propertyName: string, propertyDesc?: PropertyDescriptor) => any;
 function computedDecorator<T>(
 	targetOrOptions: Object | ICellOptions<T>,
-	name?: string,
-	desc?: PropertyDescriptor,
+	propertyName?: string,
+	propertyDesc?: PropertyDescriptor,
 	opts?: ICellOptions<T>
 ): any {
 	if (arguments.length == 1) {
-		return (target: Object, name: string, desc?: PropertyDescriptor): any =>
-			(computedDecorator as any)(target, name, desc, targetOrOptions);
+		return (target: Object, propertyName: string, propertyDesc?: PropertyDescriptor): any =>
+			(computedDecorator as any)(target, propertyName, propertyDesc, targetOrOptions);
 	}
 
-	if (desc && desc.value !== undefined && typeof desc.value != 'function') {
+	if (
+		propertyDesc &&
+		propertyDesc.value !== undefined &&
+		typeof propertyDesc.value != 'function'
+	) {
 		throw new TypeError('Invalid descriptor of computed property');
 	}
 
-	desc = (CellDecorator as any)(targetOrOptions, name as string, desc, opts);
-	(desc as PropertyDescriptor).enumerable = false;
+	propertyDesc = (CellDecorator as any)(
+		targetOrOptions,
+		propertyName as string,
+		propertyDesc,
+		opts
+	);
+	(propertyDesc as PropertyDescriptor).enumerable = false;
 
-	return desc;
+	return propertyDesc;
 }
 
 export {
