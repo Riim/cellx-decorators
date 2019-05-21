a53b0066e4b638c4643aa0e23d290813dd263ae4
Dmitry Vibe <riim@yandex.ru>
2019-05-21 10:25:29 +0300

    move NonEnumerable and Enumerable to other package

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,5 @@
 import { Cell as CellxCell, ICellOptions } from 'cellx';
 
-export function NonEnumerable(
-	_target: Object,
-	_propertyName: string,
-	propertyDesc?: PropertyDescriptor
-): any {
-	if (propertyDesc) {
-		propertyDesc.enumerable = false;
-		return propertyDesc;
-	}
-
-	return {
-		configurable: true,
-		enumerable: false,
-		writable: true,
-		value: undefined
-	};
-}
-
-export { NonEnumerable as nonEnumerable };
-
-export function Enumerable(
-	_target: Object,
-	_propertyName: string,
-	propertyDesc?: PropertyDescriptor
-): any {
-	if (propertyDesc) {
-		propertyDesc.enumerable = true;
-		return propertyDesc;
-	}
-
-	return {
-		configurable: true,
-		enumerable: true,
-		writable: true,
-		value: undefined
-	};
-}
-
-export { Enumerable as enumerable };
-
 /**
  * Babel PropertyDecorator arguments:
  * prototype
